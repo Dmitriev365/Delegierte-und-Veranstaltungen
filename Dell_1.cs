dd488873126bc76bc090fcd1b9723ff327dbdeb1
Konstantin Dmitriev <K.Dmitriev.365@hotmail.com>
2020-09-10 02:58:58 +0400

    Add_Dell_1_Dell_2

---
+++ Dell_1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegierte_und_Veranstaltungen
+{
+    class Dell_1
+    {
+        public delegate double MathDelegate(double value1, double value2);
+
+        public static double Add(double value1, double value2)
+        {
+            return value1 + value2;
+        }
+        public static double Subtract(double value1, double value2)
+        {
+            return value1 - value2;
+        }
+        Console.ReadLine();
+
+    public static void Main()
+        {
+            MathDelegate mathDelegate = Add;
+            var result = mathDelegate(5, 2);
+            Console.WriteLine(result);
+            // вывод: 7
+
+            mathDelegate = Subtract;
+            result = mathDelegate(5, 2);
+            Console.WriteLine(result);
+            // вывод: 3
+
+        }
+
+    }
--- Program.cs
+++ Program.cs
@@ -3,38 +3,5 @@
 
 class Program
 {
-    public delegate double MathDelegate(double value1, double value2);
-
-    public delegate double Sum(double value1, double value2);
-
-
-    public static double Add(double value1, double value2)
-    {
-        return value1 + value2;
-    }
-    public static double Sub(double value1, double value2)
-    {
-        return value1 - value2;
-    }
-    
-
-    public static void Main()
-    {
-
-        MathDelegate AddMathDel = new MathDelegate(Add);
-        MathDelegate SubMathDel = new MathDelegate(Sub);
-
-        Sum SumDelName = new Sum(Add);
-
-
-        var a = AddMathDel(5, 2);
-        var b = SubMathDel(5, 2);
-        var c = AddMathDel(412, 12);
-        Console.WriteLine(a);
-        Console.WriteLine(b);
-        Console.WriteLine(c);
-        // вывод: 7
-        // вывод: 3
-
-    }
+   
 }
