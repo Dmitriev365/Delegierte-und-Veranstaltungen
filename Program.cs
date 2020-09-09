04634a49aab93a543c684c14dc0ada7e1a6d4b0d
Konstantin Dmitriev <K.Dmitriev.365@hotmail.com>
2020-09-10 01:10:46 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Delegierte_und_Veranstaltungen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegierte und Veranstaltungen");
+        }
+    }
+}
