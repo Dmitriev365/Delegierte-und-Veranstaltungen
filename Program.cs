d97e2657a0b12b54bea6fa146a97f129ac661717
Konstantin Dmitriev <K.Dmitriev.365@hotmail.com>
2020-09-10 01:21:54 +0400

    Add 2x deligates

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
-namespace Delegierte_und_Veranstaltungen
+
+class Program
 {
-    class Program
+    public delegate double MathDelegate(double value1, double value2);
+
+    public static double Add(double value1, double value2)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Delegierte und Veranstaltungen");
-        }
+        return value1 + value2;
+    }
+    public static double Subtract(double value1, double value2)
+    {
+        return value1 - value2;
+    }
+    
+
+    public static void Main()
+    {
+
+
+        MathDelegate mathDelegate = Add;
+        var result = mathDelegate(5, 2);
+        Console.WriteLine(result);
+        // вывод: 7
+
+        mathDelegate = Subtract;
+        result = mathDelegate(5, 2);
+        Console.WriteLine(result);
+        // вывод: 3
+
     }
 }
